4f7daee37b3302f470f2c5a7795e1dbc3e910ab5
antillonHugo <antillonperezhugo@gmail.com>
2024-08-28 16:14:01 -0600

    Integrando componente EmptyTableAlert en las vistas de pais para informar a los usuarios cuando no hay datos disponibles en la tabla de la base de datos

--- cajero/resources/views/pais/index.blade.php
+++ cajero/resources/views/pais/index.blade.php
@@ -10,7 +10,7 @@
         </div>
         {{-- col-md-7 col-lg-5 --}}
         <div class="col-lg-6">
-            <h3 class="py-3">Listado de países</h3>
+            <h4 class="py-3">Listado de países</h4>
             <button class="btn btn-success mb-3" data-bs-toggle="modal" data-bs-target="#modalPais">
                 <i class="fa-solid fa-plus"></i> Nuevo
             </button>
@@ -20,68 +20,73 @@
         </div>
 
         <div class="col-lg-6">
-            <!-- componente para mostrar el formulario de busqueda -->
-            @component('components.forms.search-input')
-            @endcomponent
 
-            <!-- componente para mostrar las alertas -->
-            @component('components.alert.message-alerts')
-            @endcomponent
+            <h5 class="fw-bold text-center fs-2 message"></h5>
 
-            <div class="fw-bold text-center fs-2 message"></div>
+            @if ($paises->count() > 0)
+                <!-- componente para mostrar el formulario de busqueda -->
+                @component('components.forms.search-input')
+                @endcomponent
 
-            <div class="table-responsive">
-                <table class="table table-hover user-select-none cursor-default">
-                    <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">País</th>
-                            <th scope="col" class="text-end">Acciones</th>
-                        </tr>
-                    </thead>
-                    <tbody class="tbody">
-
-
-                        @foreach ($paises as $pais)
-                            <!-- Incluir el componente de modal para editar el registro de un país -->
-                            @component('components.modals.modal', ['id' => 'editModal' . $pais->cod_pais, 'title' => 'Editar País'])
-                                <!-- Contenido del formulario para editar país -->
-                                <form action="{{ route('pais.update', $pais->cod_pais) }}" method="POST">
-                                    @csrf
-                                    @method('PUT')
-                                    <!-- Campos del formulario -->
-                                    <div class="row">
-                                        <div class="col">
-                                            <div class="form-floating">
-                                                <input type="text" class="form-control" id="pais" name="pais"
-                                                    placeholder="Agregar país..." value="{{ $pais->pais }}" required>
-                                                <label for="pais">País</label>
-                                            </div>
-                                            <div class="form-group">
-                                                <input type="submit" value="Actualizar" class="btn btn-primary my-3">
+                <!-- componente para mostrar las alertas -->
+                @component('components.alert.message-alerts')
+                @endcomponent
+                <div class="table-responsive">
+                    <table class="table table-hover user-select-none cursor-default">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">País</th>
+                                <th scope="col" class="text-end">Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody class="tbody">
+                            @foreach ($paises as $pais)
+                                <!-- Incluir el componente de modal para editar el registro de un país -->
+                                @component('components.modals.modal', ['id' => 'editModal' . $pais->cod_pais, 'title' => 'Editar País'])
+                                    <!-- Contenido del formulario para editar país -->
+                                    <form action="{{ route('pais.update', $pais->cod_pais) }}" method="POST">
+                                        @csrf
+                                        @method('PUT')
+                                        <!-- Campos del formulario -->
+                                        <div class="row">
+                                            <div class="col">
+                                                <div class="form-floating">
+                                                    <input type="text" class="form-control" id="pais" name="pais"
+                                                        placeholder="Agregar país..." value="{{ $pais->pais }}" required>
+                                                    <label for="pais">País</label>
+                                                </div>
+                                                <div class="form-group">
+                                                    <input type="submit" value="Actualizar" class="btn btn-primary my-3">
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                </form>
-                            @endcomponent
+                                    </form>
+                                @endcomponent
 
-                            <!-- Incluir el componente de modal para eliminar el registro de un país -->
-                            @component('components.modals.modal', ['id' => 'deleteModal' . $pais->cod_pais, 'title' => 'Eliminar País'])
-                                <!-- Contenido del formulario para eliminar país -->
-                                <form action="{{ route('pais.destroy', $pais->cod_pais) }}" method="POST">
-                                    @csrf
-                                    @method('DELETE')
-                                    <p>Confirme si desea eliminar el país</p>
-                                    <button type="button" class="btn btn-default btn-sm border" data-bs-dismiss="modal"
-                                        aria-label="Close">Cancelar</button>
-                                    <button type="submit" class="btn btn-primary btn-sm">Confirmar</button>
-                                </form>
-                            @endcomponent
-                        @endforeach
+                                <!-- Incluir el componente de modal para eliminar el registro de un país -->
+                                @component('components.modals.modal', ['id' => 'deleteModal' . $pais->cod_pais, 'title' => 'Eliminar País'])
+                                    <!-- Contenido del formulario para eliminar país -->
+                                    <form action="{{ route('pais.destroy', $pais->cod_pais) }}" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <p>Confirme si desea eliminar el país</p>
+                                        <button type="button" class="btn btn-default btn-sm border" data-bs-dismiss="modal"
+                                            aria-label="Close">Cancelar</button>
+                                        <button type="submit" class="btn btn-primary btn-sm">Confirmar</button>
+                                    </form>
+                                @endcomponent
+                            @endforeach
 
-                    </tbody>
-                </table>
-            </div>
+                        </tbody>
+                    </table>
+                </div>
+            @else
+                <!-- componente para mostrar la alerta que la tabla no posee registros -->
+                @component('components.alert.EmptyTableAlert')
+                @endcomponent
+            @endif
+
         </div>
 
         <!-- Incluir el componente de modal para crear el registro de un país -->
