6898d9df400f79391862815999f2536946f426d2
antillonHugo <antillonperezhugo@gmail.com>
2024-08-28 22:27:30 -0600

    se Actualizo routes/api.php para incluir nuevas rutas relacionadas con la API de municipios

--- cajero/routes/api.php
+++ cajero/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\PaisController;
 use App\Http\Controllers\Api\DepartamentosController;
+use App\Http\Controllers\api\MunicipioController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -12,3 +13,4 @@ Route::get('/user', function (Request $request) {
 // Define la ruta para los end-point
 Route::get('pais/{searchText?}', [PaisController::class, 'index']);
 Route::get('departamento/{searchText?}', [DepartamentosController::class, 'index']);
+Route::get('municipio/{searchText?}', [MunicipioController::class, 'index']);
