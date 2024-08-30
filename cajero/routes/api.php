d568123d7c2025bad40e45ff219b198a556a3471
antillonHugo <antillonperezhugo@gmail.com>
2024-08-30 00:20:59 -0600

    define una ruta para implementar una funcionalidad de búsqueda mediante fetch

--- cajero/routes/api.php
+++ cajero/routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\PaisController;
 use App\Http\Controllers\Api\DepartamentosController;
 use App\Http\Controllers\api\MunicipioController;
+use App\Http\Controllers\api\ClienteController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -14,3 +15,4 @@ Route::get('/user', function (Request $request) {
 Route::get('pais/{searchText?}', [PaisController::class, 'index']);
 Route::get('departamento/{searchText?}', [DepartamentosController::class, 'index']);
 Route::get('municipio/{searchText?}', [MunicipioController::class, 'index']);
+Route::get('cliente/{searchText?}', [ClienteController::class, 'index']);
