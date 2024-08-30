ce4135bcdb52bcd127b9dcec6b2eaaf75919d44f
antillonHugo <antillonperezhugo@gmail.com>
2024-08-30 00:26:04 -0600

    se realizaron algunos ajustes con el objetivo de hacer un metodo mas generalizado el cual me permite recoorrer la respuesta json

--- cajero/public/js/searchForm.js
+++ cajero/public/js/searchForm.js
@@ -15,6 +15,7 @@ if (document.getElementById("searchText")) {
     // Agrega el evento solo si el elemento existe
     searchText[0].addEventListener("keyup", function () {
         searchTextvalue = searchText[0].value.trim();
+        console.log(searchTextvalue);
         buscadorApi(solicitudApi, searchTextvalue);
     });
 }
@@ -24,12 +25,13 @@ function obtenersolicitudfetch() {
     //obtengo el ultimo valor de la url
     let segmento = URLactual.split("/");
     solicitudApi = segmento.at(-1);
+    console.log(solicitudApi);
     return solicitudApi;
 }
 
 //esta función realiza una búsqueda en una API
 function buscadorApi(solicitudApi, searchTextvalue) {
-    if (solicitudApi != "") {
+    if (solicitudApi != "" && solicitudApi != "clientes-por-pais") {
         // Codifica el texto de búsqueda para evitar problemas con caracteres especiales
         let encodedSearchText = encodeURIComponent(searchTextvalue);
         // creamos la URL para incluir el parámetro 'searchText'
@@ -51,11 +53,13 @@ function buscadorApi(solicitudApi, searchTextvalue) {
                 if (data.message) {
                     // Mostramos el mensaje en la interfaz de usuario
                     table_responsive[0].classList.add("d-none");
+                    // Asigna la clase de Bootstrap
+                    message[0].classList.remove("d-none");
                     message[0].innerHTML = data.message;
                 } else {
                     message[0].innerHTML = "";
+                    message[0].classList.add("d-none");
                     table_responsive[0].classList.remove("d-none");
-
                     //enviamos los datos
                     tabla(data);
                 }
@@ -66,32 +70,36 @@ function buscadorApi(solicitudApi, searchTextvalue) {
     }
 }
 
-//esta función permitirá mostrar los registros en una tabla
 function tabla(data) {
-    //objeto JSON con una propiedad 'paises'
-    let datos = data[solicitudApi];
+    // Obtenemos la primera clave del objeto para acceder a los datos
+    let datos = data[Object.keys(data)[0]];
 
     let content_tbody = "";
 
     datos.forEach((dto) => {
+        content_tbody += "<tr>";
+        // Iteramos sobre cada propiedad del objeto
+        for (let key in dto) {
+            if (dto.hasOwnProperty(key)) {
+                content_tbody += `<td>${dto[key]}</td>`;
+            }
+        }
         content_tbody += `
-        <tr>
-            <th scope="row">${dto["cod_" + solicitudApi]}</th> 
-                <td class="text-capitalize">${dto[solicitudApi]}</td>
-                <td class="text-end"> 
-                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editModal${
-                        dto["cod_" + solicitudApi]
-                    }">
-                        <i class="fa-regular fa-pen-to-square"></i>
-                    </button>
-                    <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#deleteModal${
-                        dto["cod_" + solicitudApi]
-                    }">
-                        <i class="fa-solid fa-trash"></i>
-                    </button>
-                </td>  
+            <td class="text-end"> 
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editModal${
+                    dto["cod_" + solicitudApi]
+                }">
+                    <i class="fa-regular fa-pen-to-square"></i>
+                </button>
+                <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#deleteModal${
+                    dto["cod_" + solicitudApi]
+                }">
+                    <i class="fa-solid fa-trash"></i>
+                </button>
+            </td>
         </tr>`;
     });
-    // Asigna la cadena al elemento con el cada clase "tbody"
-    tbody[0].innerHTML = content_tbody;
+
+    // Asigna la cadena al elemento con la clase "tbody"
+    document.querySelector(".tbody").innerHTML = content_tbody;
 }
