5d61222e6f9de44499145105052558a3ad69a053
antillonHugo <antillonperezhugo@gmail.com>
2024-08-30 00:09:04 -0600

    definimos una ruta de recurso para el controlador ClienteController

--- cajero/routes/web.php
+++ cajero/routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PaisController;
 use App\Http\Controllers\DepartamentoController;
 use App\Http\Controllers\MunicipioController;
+use App\Http\Controllers\ClienteController;
 use Illuminate\Support\Facades\URL;
 
 // URL::forceScheme('https');
@@ -13,8 +14,8 @@ Route::get('/', function () {
     return view('home.index');
 })->name('home');
 
-
 // Definimos todas las rutas para realizar el CRUD
 Route::resource('pais', PaisController::class)->middleware(XssSanitization::class);
 Route::resource('departamento', DepartamentoController::class)->middleware(XssSanitization::class);
 Route::resource('municipio', MunicipioController::class)->middleware(XssSanitization::class);
+Route::resource('cliente', ClienteController::class)->middleware(XssSanitization::class);
