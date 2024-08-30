013b24076e7d32466c01326d12c80c96692ceb16
antillonHugo <antillonperezhugo@gmail.com>
2024-08-29 23:55:06 -0600

    se creo un contenedor especifico para el formulario de clientes ya que este contiene mas funcionalidad

---
+++ cajero/resources/views/components/contenedor-secundario.blade.php
@@ -0,0 +1,9 @@
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-10">
+            <div class="row shadow mt-3 mt-md-5 px-md-3">
+                {{ $slot }}
+            </div>
+        </div>
+    </div>
+</div>
