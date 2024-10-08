c4b9d467fd6635ec758fd834c3dcf7aa54b8b95c
antillonHugo <antillonperezhugo@gmail.com>
2024-10-08 16:18:30 -0600

    se modificaron nuevas reglas para manejar la validacionóó de las solicitudes HTTP a nuestro ClienteController

--- cajero/app/Http/Requests/ClienteFormRequest.php
+++ cajero/app/Http/Requests/ClienteFormRequest.php
@@ -26,13 +26,34 @@ class ClienteFormRequest extends FormRequest
             'segundo_nombre' => 'string|max:25',
             'primer_apellido' => 'required|string|max:25',
             'segundo_apellido' => 'string|max:25',
-            'dui' => 'required|int|max:9|unique:clientes',
-            'fecha_de_nacimiento' => 'required|string',
-            'celular' => 'required|int|max:9|unique:clientes',
-            'correo' => 'required|string|max:50|unique:clientes',
-            'cod_pais' => 'required|int|max:9|exists:paises,cod_pais',
+            'dui' => 'required|size:10|unique:clientes,dui',
+            'fecha_de_nacimiento' => 'required|date',
+            'celular' => 'required|size:9|unique:clientes',
+            'correo' => 'required|max:50|email|unique:clientes,correo',
+            'cod_pais' => 'required|int|exists:paises,cod_pais',
             'cod_departamento' => 'required|int|exists:departamentos,cod_departamento',
             'cod_municipio' => 'required|int|exists:municipios,cod_municipio'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'nombre.required' => 'El campo nombre es obligatorio.',
+            'primer_apellido.required' => 'El campo primer apellido es obligatorio.',
+            'dui.required' => 'El campo DUI es obligatorio.',
+            'dui.unique' => 'El DUI ya está registrado.',
+            'fecha_de_nacimiento.required' => 'El campo Fecha de Nacimiento es obligatorio.',
+            'celular.required' => 'El campo celular es obligatorio.',
+            'celular.unique' => 'El celular ya está registrado.',
+            'correo.required' => 'El campo correo es obligatorio.',
+            'correo.unique' => 'El correo ya está registrado.',
+            'cod_pais.required' => 'El campo país es obligatorio.',
+            'cod_pais.exists' => 'El país no es válido.',
+            'cod_departamento.required' => 'El campo departamento es obligatorio.',
+            'cod_departamento.exists' => 'El departamento no es válido.',
+            'cod_municipio.required' => 'El campo municipio es obligatorio.',
+            'cod_municipio.exists' => 'El municipio no es válido.',
         ];
     }
 }
