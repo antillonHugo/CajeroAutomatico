72cc7630433444647d338cadf813f3ecc2be384b
antillonHugo <antillonperezhugo@gmail.com>
2024-10-10 21:40:47 -0600

    se modifico el componente adaptandolo para que muestre la alerta segun la necesidad este puede ser cuando una operacion se realizo con exito como tambien si la operacion falla

--- cajero/resources/views/components/alert/alert-toast.blade.php
+++ cajero/resources/views/components/alert/alert-toast.blade.php
@@ -1,4 +1,4 @@
-<div class="toast-container position-fixed bottom-0 end-0 p-3">
+{{-- <div class="toast-container position-fixed bottom-0 end-0 p-3">
     <div class="toast align-items-center text-bg-{{ $tipo }} border-0" role="alert" aria-live="assertive"
         aria-atomic="true">
         <div class="d-flex">
@@ -9,4 +9,32 @@
                 aria-label="Close"></button>
         </div>
     </div>
+</div> --}}
+
+<div class="toast-container position-fixed bottom-0 end-0 p-3">
+    @if (session('success'))
+        <div class="toast align-items-center text-bg-success border-0" role="alert" aria-live="assertive"
+            aria-atomic="true">
+            <div class="d-flex">
+                <div class="toast-body">
+                    {{ session('success') }}
+                </div>
+                <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast"
+                    aria-label="Close"></button>
+            </div>
+        </div>
+    @endif
+
+    @if (session('error'))
+        <div class="toast align-items-center text-bg-danger border-0" role="alert" aria-live="assertive"
+            aria-atomic="true">
+            <div class="d-flex">
+                <div class="toast-body">
+                    {{ session('error') }}
+                </div>
+                <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast"
+                    aria-label="Close"></button>
+            </div>
+        </div>
+    @endif
 </div>
